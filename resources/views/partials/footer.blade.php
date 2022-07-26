de6e2b0b0a0df7c116e78e72c87d575029eebfa0
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-26 20:28:50 +0200

    sign-up section completed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -49,3 +49,4 @@
         </div>
     </div>
 </div>
+@include('partials.sign_up')
---
+++ resources/views/partials/sign_up.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <div class="container-fluid">
+        <div class="row bg-secondary custom-style-banner">
+
+            <div class="col">
+                <button class="btn btn-outline-primary text-uppercase button-custom-style">Sign Up Now!</button>
+            </div>
+            <div class="col d-flex justify-content-end align-items-center gap-3 custom-style-link-banner">
+                <a class="text-uppercase" href="#">Follow us</a>
+                <img src="{{ asset('images/footer-facebook.png') }}" alt="">
+                <img src="{{ asset('images/footer-twitter.png') }}" alt="">
+                <img src="{{ asset('images/footer-youtube.png') }}" alt="">
+                <img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+                <img src="{{ asset('images/footer-periscope.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+</div>
