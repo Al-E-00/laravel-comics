8114a5bd5a02d05a42560731918e8beb290d3760
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-26 17:49:35 +0200

    header img not works

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,8 +1,9 @@
-<div class="card">
-    <img src='{{ $comic['thumb'] }}' class="card-img-top" alt="...">
+<div class="card custom-card-style">
+    <div class="card-image-container">
+        <img src='{{ $comic['thumb'] }}' class="card-img-top" alt=" {{ $comic['thumb'] }} ">
+    </div>
 
-    <div class="card-body">
-
+    <div class="custom-body-style">
         <h5 class="card-title"> {{ $comic['title'] }} </h5>
     </div>
 </div>
