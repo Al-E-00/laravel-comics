3d9c67158050799645c09a9536a99baa03ab898a
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-26 20:49:13 +0200

    bg jumbotron done

--- resources/views/page_content.blade.php
+++ resources/views/page_content.blade.php
@@ -2,18 +2,23 @@
 
 @section('page_content')
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col">
-            <div class="full-background-jumbotron">
-            </div>
-        </div>  
-        <div class="row row-cols-6">
-                @foreach($comics as $comic)
+<div>
+    <div class="row background-jumbotron">
+        
+    </div>
+    <div class="row row-cols-6 main-jumbotron">
+        <div class="banner-custom bg-primary">
+            <h5 class="text-uppercase m-0">Current series</h5>
+        </div>
+        <div class="col g-3 p-0 d-flex justify-content-center">
+            @foreach($comics as $comic)
                     @include('partials.cards', [
                         "comic" => $comic
                     ])
-                @endforeach
+            @endforeach
+        </div>
+        <div class="banner-custom-load-more bg-primary">
+            <h5 class="text-uppercase m-0">Load More</h5>
         </div>
     </div>
 </div>
--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,9 +1,11 @@
-<div class="card custom-card-style">
-    <div class="card-image-container">
-        <img src='{{ $comic['thumb'] }}' class="card-img-top" alt=" {{ $comic['thumb'] }} ">
-    </div>
+<div>
+    <div class="container">
+        <div class="card-custom-style">
+            <img src='{{ $comic['thumb'] }}' alt='{{ $comic['thumb'] }}'>
+        </div>
+        <div>
+            <p class="text-card-custom-style">{{ $comic['title'] }}</p>
+        </div>
 
-    <div class="custom-body-style">
-        <h5 class="card-title"> {{ $comic['title'] }} </h5>
     </div>
 </div>
