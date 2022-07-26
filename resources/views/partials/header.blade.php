2daf72266847473908c43bfb0edb6425a01dc18d
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-26 14:49:23 +0200

    Added more structure and descriptions

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <a href="#">Dc power<span>sm</span> visa<span><i class="fa-regular fa-registered"></i></span></a>
+        </div>
+    </div>
+</div>
