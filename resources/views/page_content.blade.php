03fb275c1c39ed7a9be569e48cb42cb1a7ff5691
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-26 15:54:03 +0200

    url('images/jumbotron.jpg'); -> path for bg edited -- still not works

--- resources/views/page_content.blade.php
+++ resources/views/page_content.blade.php
@@ -1,9 +1,20 @@
+@imoport()
+
 <div class="container-fluid">
     <div class="row">
         <div class="col">
             <div class="full-background-jumbotron">
-                
+
             </div>
         </div>  
+        <div class="row">
+            <div class="col">
+                @foreach ($products as $product)
+                @include('partials.productCard', [
+                    "prodotto" => $product
+                ])
+                @endforeach
+            </div>
+        </div>
     </div>
 </div>
