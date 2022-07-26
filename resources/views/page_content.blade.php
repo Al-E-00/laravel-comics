72278eb3df2e5a95722dba71cdcc3b2c83f0f39a
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-26 17:22:11 +0200

    card visualized on screen

--- resources/views/page_content.blade.php
+++ resources/views/page_content.blade.php
@@ -8,10 +8,12 @@
             <div class="full-background-jumbotron">
             </div>
         </div>  
-        <div class="row">
-            <div class="col">
-                @dump($comics)
-            </div>
+        <div class="row row-cols-6">
+                @foreach($comics as $comic)
+                    @include('partials.cards', [
+                        "comic" => $comic
+                    ])
+                @endforeach
         </div>
     </div>
 </div>
