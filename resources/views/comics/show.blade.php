c854c2f80be426b5840992134e6fbfc8f91820f7
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-27 18:53:15 +0200

    basic layout completed for the new page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,34 +1,115 @@
+@php
+$banner_icons = [
+    [
+        "name" => "digital comics",
+        "img" => "images/buy-comics-digital-comics.png"
+    ],
+    [
+        "name" => "shop dc",
+        "img" => "images/buy-comics-subscriptions.png"
+    ],
+    [
+        "name" => "comic shop locator",
+        "img" => "images/buy-comics-shop-locator.png"
+    ],
+    [
+        "name" => "merchandise",
+        "img" => "images/buy-comics-merchandise.png"
+    ],
+];
+
+@endphp
+
 @extends('layouts.app')
 
 @section('page_title', $comic['title'] . 'comic detail')
 
 @section('page_content')
 
-<div class="container-fluid">
-    <div class="row background-jumbotron">
-        
-    </div>
-    <div class="row">
-        <div class="col custom-comic-image-section p-0">
-            <img class="custom-single-comic-image" src="{{ asset($comic['thumb']) }}" alt="">
-            <div class="custom-division-line-header-comic"></div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <h4>{{ $comic['title'] }}</h4>
-            <div class="check-availability-banner">
-                <span>U.S. Price: {{ $comic['price'] }}</span>
-                <span> {{ $comic['availability'] }} </span>
-                <button>Check Availability</button>
-            </div>
-            <span> {{ $comic['description'] }} </span>
-        </div>
-        <div class="col">
-            <h6>Advertisement</h6>
-            <img src="{{ asset('images/adv.jpg') }}" alt="">
-        </div>
-    </div>
-<div>
+    <div class="container-fluid">
+        <div class="row background-jumbotron">
 
-@endsection
+        </div>
+        <div class="row">
+            <div class="col custom-comic-image-section p-0">
+                <img class="custom-single-comic-image" src="{{ asset($comic['thumb']) }}">
+                <div class="custom-division-line-header-comic"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h6>Talent</h6>
+                <div>
+                    <span class="title-descriptive-section">
+                        Art by:
+                    </span>
+                    <span class="content-descriptive-section">
+                        @foreach ($comic['artists'] as $artist)
+                            {{ $artist }}
+                    </span>
+                    @if (!$loop->last)
+                        <span>,</span>
+                    @endif
+                    @endforeach
+                </div>
+
+                <div>
+                    <span class="title-descriptive-section">
+                        Written by:
+                    </span>
+                    <span class="content-descriptive-section">
+                        @foreach ($comic['writers'] as $writer)
+                            {{ $writer }}
+                    </span>
+    
+                    @if (!$loop->last)
+                        <span>,</span>
+                    @endif
+                    @endforeach
+                </div>
+
+
+            </div>
+            <div class="col">
+                <h6>Specs</h6>
+                <div>
+                    <span class="title-descriptive-section">
+                        Series: 
+                    </span>
+                    <span class="content-descriptive-section">
+                        {{ $comic['series'] }}
+                    </span>
+                </div>
+                <div>
+                    <span class="title-descriptive-section">
+                        U.S. Price:
+                    </span>
+                    <span class="content-descriptive-section">
+                        {{ $comic['price']}}
+                    </span>
+                </div>
+                <div>
+                    <span class="title-descriptive-section">
+                        On Sale Date:
+                    </span>
+                    <span>
+                        {{ $comic['sale_date'] }}
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col d-flex">
+                @foreach($banner_icons as $banner)
+                <div>
+                    <span>
+                        {{ $banner['name'] }}
+                    </span>
+                    <img src="{{ asset($banner['img']) }}" alt="">
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <div>
+
+        @endsection
