f58a108800e7be6c983932d27d8acb001d5ab4cd
Al-E-00 <mazzellaalessio6@gmail.com>
2022-07-27 15:49:39 +0200

    data correctly linked between detail page and web.php page

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function() {
     $comics = config("comics");
 
-    return view('page_content', compact("comics"));
+    return view('comics.index', compact("comics"));
 })->name("comics");
 
+
+Route::get("/comics/{id}", function($id) {
+    /* The config method allows your application to get 
+    or set values in all files that are in the config directory. */
+
+    $comics = config("comics");
+
+    /* The array_filter() function filters the values of an array 
+    using a callback function. This function passes each value of 
+    the input array to the callback function.  If the callback function 
+    returns true, the current value from input is returned into the result 
+    array. Array keys are preserved.*/
+
+
+
+    $foundComics = array_filter($comics, function ($comic) use($id) {
+        return $comic["id"] === ((int) $id);
+    });
+
+    $keys = array_keys($foundComics)[0];
+
+    $comic = $foundComics[$keys];
+
+    return view("comics.show", compact("comic"));
+});
